ad615265abc5b6d3eb99b3adf4c62156dcef753c
Stefan Tcaciuc <stefantcaciuc@pop-os.localdomain>
2023-02-20 20:23:08 +0000

    Defined a simple API route for testing purposes and added the CORS middle-ware to allow cross-origin requests. Added built in express.json() method for parsing incoming JSON data.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import { Request, Response } from 'express';
 
+// Create Express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// Use CORS middleware
+app.use(cors());
+
+// Parse JSON and URL-encoded query string data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define API routes
+app.get('/api', (req: Request, res: Response) => {
+  res.send('API is working');
 });
 
-app.listen(9000, () => {
-  console.log("Server listening on port 3000");
+// Start the server
+const port = 9000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
