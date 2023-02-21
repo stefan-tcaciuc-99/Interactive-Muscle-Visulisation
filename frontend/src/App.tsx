6d8f3dbc735de897e12eb267f1f6f37ca09b1037
Stefan Tcaciuc <stefantcaciuc@pop-os.localdomain>
2023-02-21 16:29:06 +0000

    setup a demo react frontend button to send a message to server for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleClick = async () => {
+    const response = await axios.get('/api');
+    setMessage(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Send message to server</button>
+      <p>{message}</p>
     </div>
   );
 }
