6635a1da121051cec8d22229122b8252234695dc
Stefan Tcaciuc <stefantcaciuc@pop-os.localdomain>
2023-02-21 20:14:06 +0000

    implemented modelviewer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,37 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import "@google/model-viewer/dist/model-viewer.min.js";
+import "./app.css";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const handleClick = async () => {
-    const response = await axios.get('/api');
+    const response = await axios.get("/api");
     setMessage(response.data);
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>Send message to server</button>
-      <p>{message}</p>
+    // <body>
+    <div className="parent">
+      <div className="div1">
+        <model-viewer
+          className="model-viewer"
+          src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+          alt="A 3D model of an astronaut"
+          camera-controls
+          exposure="0.6"
+          shadow-intensity="1.5"
+          orbit="45deg 55deg 4m"
+        />
+      </div>
+      <div className="div2"> </div>
+      <div className="div3">
+        <button onClick={handleClick}>Send message to server</button>
+        <p>{message}</p>{" "}
+      </div>
     </div>
+    // </body>
   );
 }
 
