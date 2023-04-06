0c6349b66c0310186dcabee40b33e0aff27dd8ac
Stefan Tcaciuc <stefan.tcaciuc.99@gmail.com>
2023-04-06 19:21:02 +0100

    implement login page

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword} from "firebase/auth";
+import auth from "../firebase";
+import { styled } from "@mui/system";
+import { TextField, Button, Container, Typography } from "@mui/material";
+
+const FormContainer = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100vh",
+});
+
+const LoginForm = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+});
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error:any) {
+      alert(`Error: ${error.message}`);
+    }
+  };
+
+  return (
+    <FormContainer maxWidth="xs">
+      <Typography variant="h4" mb={4}>
+        Login
+      </Typography>
+      <LoginForm onSubmit={handleSubmit}>
+        <TextField
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <TextField
+          type="password"
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </LoginForm>
+    </FormContainer>
+  );
+};
+
+export default Login;
