214fcd967d8662b87c72acbd7d9f4cee871a2416
Stefan Tcaciuc <stefan.tcaciuc.99@gmail.com>
2023-04-08 22:48:49 +0100

    added the exercice type selector to homepage and styled it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,31 +6,79 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import AuthButtons from "./components/AuthButtons";
 import "./app.css";
-import { Grid } from '@mui/material';
+import { Grid, Paper } from "@mui/material";
+import ExerciseTypeSelector from "./components/ExerciseTypeSelector";
+import useExerciseData from "./hooks/useExerciseData";
+import { exerciseTypes } from "./services/exerciseService";
 
 function HomePage() {
   const modelViewerElementRef = useModelViewerRef();
-  
+
+  const { selectedExerciseType, selectExerciseType } =
+    useExerciseData(exerciseTypes);
 
   return (
     <div className="parent">
-      <Grid container sx={{ height: '100vh', padding: '16px' }}>
-        <Grid item xs={12} className="div4" sx={{ marginBottom: '16px', backgroundColor: 'rgba(128, 0, 128, 0.2)' }}></Grid>
-        <Grid item xs={12} sm={8} md={9} sx={{ marginBottom: '16px', display: 'flex', alignItems: 'center', backgroundColor: 'rgba(255, 0, 0, 0.2)' }}>
-          <ModelViewer modelViewerRef={modelViewerElementRef} />
-        </Grid>
-        <Grid item xs={12} sm={4} md={3} className="div2" sx={{ marginBottom: '16px', backgroundColor: 'rgba(0, 255, 0, 0.2)', position: 'relative' }}>
+      <Grid container sx={{ height: "100vh", padding: "16px" }}>
+        <Grid
+          item
+          xs={12}
+          className="div4"
+          sx={{
+            marginBottom: "16px",
+            backgroundColor: "rgba(128, 0, 128, 0.2)",
+          }}
+        ></Grid>
+        <Grid
+  item
+  xs={12}
+  sm={8}
+  md={9}
+  sx={{
+    marginBottom: "16px",
+    display: "flex",
+    alignItems: "flex-start",
+    backgroundColor: "rgba(255, 0, 0, 0.2)",
+    paddingTop: "16px",
+    paddingLeft: "16px",
+  }}
+>
+  <ModelViewer modelViewerRef={modelViewerElementRef} />
+  <Paper sx={{ padding: "16px", marginLeft: "16px" }}>
+    <ExerciseTypeSelector
+      exerciseTypes={exerciseTypes}
+      selectedType={selectedExerciseType}
+      onSelect={selectExerciseType}
+    />
+  </Paper>
+</Grid>
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          md={3}
+          className="div2"
+          sx={{
+            marginBottom: "16px",
+            backgroundColor: "rgba(0, 255, 0, 0.2)",
+            position: "relative",
+          }}
+        >
           <header className="header">
             <AuthButtons />
           </header>
+          
         </Grid>
-        <Grid item xs={12} className="div3" sx={{ backgroundColor: 'rgba(0, 0, 255, 0.2)' }}></Grid>
+        <Grid
+          item
+          xs={12}
+          className="div3"
+          sx={{ backgroundColor: "rgba(0, 0, 255, 0.2)" }}
+        ></Grid>
       </Grid>
     </div>
   );
 }
-
-
 
 function App() {
   return (
--- frontend/src/components/ExerciseTypeSelector.tsx
+++ frontend/src/components/ExerciseTypeSelector.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import {List, ListItem, ListItemText} from '@mui/material';
+import React from "react";
+import { Button, Box } from "@mui/material";
 
 interface ExerciseTypeSelectorProps {
-    exerciseTypes: string[];
-    selectedType: string|null;
-    onSelect: (type: string) => void;
+  exerciseTypes: string[];
+  selectedType: string | null;
+  onSelect: (type: string) => void;
 }
 
-const ExerciseTypeSelector: React.FC<ExerciseTypeSelectorProps> = ({exerciseTypes, selectedType, onSelect}) => {
-    return (
-        <List>
-            {exerciseTypes.map((type) => (
-                <ListItem
-                    key={type}
-                    button
-                    selected={type === selectedType}
-                    onClick={() => onSelect(type)}
-                >
-                    <ListItemText primary={type} />
-                </ListItem>
-            ))}
-        </List>
-    );
+const ExerciseTypeSelector: React.FC<ExerciseTypeSelectorProps> = ({
+  exerciseTypes,
+  selectedType,
+  onSelect,
+}) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      flexWrap="wrap"
+      gap={1}
+    >
+      {exerciseTypes.map((type) => (
+        <Button
+          key={type}
+          onClick={() => onSelect(type)}
+          variant={selectedType === type ? "contained" : "outlined"}
+          color={selectedType === type ? "primary" : undefined}
+        >
+          {type}
+        </Button>
+      ))}
+    </Box>
+  );
 };
 
 export default ExerciseTypeSelector;
